b7c55df05fd63861ef74e9b24f016eeb283d4b9c
benjahjp <benjahjp@gmail.com>
2023-04-20 13:47:56 +0300

    feat: data layer

--- app/src/main/java/com/example/punk/data/mapper/BeerMappers.kt
+++ app/src/main/java/com/example/punk/data/mapper/BeerMappers.kt
@@ -1,2 +1,27 @@
 package com.example.punk.data.mapper
 
+import com.example.punk.data.local.BeerEntity
+import com.example.punk.data.remote.dto.BeerDto
+import com.example.punk.domain.model.Beer
+
+fun BeerDto.toBeerEntity(): BeerEntity{
+    return BeerEntity(
+        id = id,
+        name = name,
+        tagLine = tagLine,
+        description = description,
+        first_brewed = first_brewed,
+        image_url = image_url
+    )
+}
+
+fun BeerEntity.toBeer(): Beer {
+    return Beer(
+        id = id,
+        name = name,
+        tagLine = tagLine,
+        description = description,
+        first_brewed = first_brewed,
+        image_url = image_url
+    )
+}
--- app/src/main/java/com/example/punk/domain/model/Beer.kt
+++ app/src/main/java/com/example/punk/domain/model/Beer.kt
@@ -6,5 +6,5 @@ data class Beer(
     val tagLine: String,
     val description: String,
     val first_brewed: String,
-    val image_url: String
+    val image_url: String?
 )
